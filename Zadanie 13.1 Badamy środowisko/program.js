8dfbf676f972b5733ef5f203087757fe76ced6d0
Maciej Kubas <maciekkubas@gmail.com>
2018-04-23 17:34:19 +0200

    corrected

--- Zadanie 13.1 Badamy środowisko/program.js
+++ Zadanie 13.1 Badamy środowisko/program.js
@@ -3,17 +3,30 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
 
     var input = process.stdin.read();
+    var help = '/info - node version\n/lang - show environment\n/exit - exit nodes.js';
+    
+    console.log(help);
 
     if (input !== null) {
         var instruction = input.toString().trim();
+        switch (instruction) {
 
-        switch (instruction === '/exit') {
-
-           case true:
+           case ('/exit'):
 
             process.stdout.write('Quitting app!\n')
             process.exit();
+            break;
+
+            case ('/info'):
+
+            process.stdout.write('Node:\n')
+            console.log(`Version: ${process.version}`);
+            break;
             
+            case('/lang'): 
+
+            process.stdout.write('Environment:\n')
+            console.log(process.env);
             break;
 
          default:
@@ -21,5 +34,3 @@ process.stdin.on('readable', function() {
         }
     }
 });
-
-console.log(process.env);
