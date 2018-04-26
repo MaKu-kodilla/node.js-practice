4593578aeaa51210c112d721fdc50d766d72d214
Maciej Kubas <maciekkubas@gmail.com>
2018-04-26 21:01:03 +0200

    task-completed

---
+++ Zadanie 13.2 Kształtujemy czas/modules/OStime.js
@@ -0,0 +1,13 @@
+
+var os = require('os');
+
+function getOStime (uptime) {
+    var hoursTotal = uptime / 60 / 60;
+    var minutesTotal = uptime / 60 % 60;
+    var secondsLeft = uptime % 60;
+
+    return Math.floor(hoursTotal) + ' h ' + minutesTotal.toFixed(0) + ' min ' + secondsLeft.toFixed(0) + ' sec';
+
+}
+
+exports.print = getOStime;
