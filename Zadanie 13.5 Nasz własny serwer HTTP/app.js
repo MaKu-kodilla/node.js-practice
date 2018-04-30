065835e090041df834571534facbdc6b07b5e9e5
Maciej Kubas <maciekkubas@gmail.com>
2018-04-30 22:17:51 +0200

    task-completed

---
+++ Zadanie 13.5 Nasz własny serwer HTTP/app.js
@@ -0,0 +1,25 @@
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/hello') {
+        fs.readFile('index.html', function (err, data) {
+            if(err) throw err;
+                response.statusCode = 404;
+                response.end(data);
+        })
+    } else {
+        response.setHeader("Content-Type", "image/jpg");
+        fs.readFile('closed-img.jpg', function (err, data) {
+            if (err) throw err;
+                response.statusCode = 404;
+                response.end(data);
+        })
+    }
+});
+
+server.listen(9000);
